1faa6a1674d544d55325d4ff1fae6ec295124b32
Keith Williams <mrkeithwilliams@gmail.com>
2017-11-27 20:56:19 -0500

    first failing test

--- fizzbuzz/test/test.js
+++ fizzbuzz/test/test.js
@@ -1,9 +1,17 @@
 import Mocha from "mocha";
 import { expect } from "chai";
-
+import { fizzbuzz } from "../src/index";
 
 describe("Canary Test", ()=> {
     it("true equals true", ()=> {
         expect(true).to.equal(true);
     })
 })
+
+describe("fizzbuzz kata", () => {
+    describe("fizzbuzz input", () => {
+        it("returns input", () =>{
+            expect(fizzbuzz()).to.equal(1);
+        })
+    })
+}) 
